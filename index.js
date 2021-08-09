02e53cbbc83a5452d193d8b404860ef6141c000e
Glitch (glitch-hello-node) <none>
2021-08-09 10:19:27 +0000

    💇🛍 Checkpoint ./package.json:25920309/520 ./index.js:5358170/242:25920309/1190

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("31")
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  client.channels.cache.get("874062459303002152").send("hazir");
+});
+
+client.on("messageDelete",message=> {
+  console.log(message)
+});
+
+client.login('ODQ1NDI4MDIyMTgxMzYzNzMz.YKg0Mw.cWFQbU2YMYYiO3c_fyYChND7UL4');

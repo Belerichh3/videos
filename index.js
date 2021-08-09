2c0f5711e3258880870c656915cb6739c9731db3
Glitch (glitch-hello-node) <none>
2021-08-09 10:43:26 +0000

    🌂🍂 Checkpoint ./index.js:25920309/3298

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-const { Client, Intents , Events} = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILD_MESSAGES] });
+const { Client, Intents , Collection} = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS,Intents.FLAGS.GUILD_MEMBERS,Intents.FLAGS.GUILD_BANS,Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,Intents.FLAGS.GUILD_INTEGRATIONS,Intents.FLAGS.GUILD_WEBHOOKS,Intents.FLAGS.GUILD_INVITES,Intents.FLAGS.GUILD_VOICE_STATES,Intents.FLAGS.GUILD_PRESENCES,Intents.FLAGS.GUILD_MESSAGES,Intents.FLAGS.GUILD_MESSAGE_REACTIONS,Intents.FLAGS.GUILD_MESSAGE_TYPING,Intents.FLAGS.DIRECT_MESSAGES,Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,Intents.FLAGS.DIRECT_MESSAGE_TYPING] });
+const fs = require("fs");
+
+client.commands = new Collection(); 
+client.aliases = new Collection();
+client.categories = fs.readdirSync("./commands/"); //categories
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -7,7 +12,7 @@ client.on('ready', () => {
 });
 
 client.on("messageCreate",message=> {
-  console.log("123")
+  console.log(message.content)
 });
 client.login("ODQ1NDI4MDIyMTgxMzYzNzMz.YKg0Mw.cWFQbU2YMYYiO3c_fyYChND7UL4")
 

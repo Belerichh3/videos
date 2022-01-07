b9f466c65c3818471ecf4f68da2405e7829eaaa4
Glitch (glitch-hello-node) <none>
2022-01-07 17:03:39 +0000

    📟🍍 Checkpoint ./ardademrbotunkalbi/komut.js:25915949/985 ./index.js:25915949/1950

--- ardademrbotunkalbi/komut.js
+++ ardademrbotunkalbi/komut.js
@@ -1,22 +1,3 @@
-const { readdirSync } = require("fs"); 
-const ascii = require("ascii-table");
-
-let table = new ascii("Komutlar");
-table.setHeading("Komut", "Yükleme durumu");
-
-module.exports = (client) => {
-        const commands = readdirSync(`./ardademrkomutlar/`).filter(file => file.endsWith(".js")); 
-        for (let file of commands) { 
-            let pull = require(`../ardademrkomutlar/${file}`); 
-            if (pull.isim) { 
-                client.komutlar.set(pull.isim, pull); 
-                table.addRow(file, 'Hazır'); 
-            } else {
-                table.addRow(file, `Hata -> Komut klasöründe isim yazılmamış.`); 
-                continue; 
-            }
-            if (pull.alternatifler && Array.isArray(pull.alternatifler)) pull.alternatifler.forEach(alias => client.alternatifler.set(alias, pull.isim)); 
-        }
-    console.log(table.toString()); 
-
-}
+client.on("ready", () => {
+  client.channels.get("928997008323264572").join();
+})
